052fcfc957a197d52b4055b0cfa2d4d334a2dd91
MarcAndreFL <marcfl20@gmail.com>
2016-02-10 20:53:10 -0500

    Ça avance bien

--- intro-bd-tp1/intro-bd-tp1/Main.cpp
+++ intro-bd-tp1/intro-bd-tp1/Main.cpp
@@ -1,5 +1,9 @@
 #include "Database.h"
+#include <iostream>
 
 void main(){
-
+	Book myBook =  Book(1, "allo", "hello", 1994);
+	Database::GetInstance()._bookTable.insert(std::pair<unsigned int, Book>(myBook.GetISBN(), myBook));
+	std::cout << Database::GetInstance()._bookTable.at(myBook.GetISBN()).GetTitle();
+	system("PAUSE");
 }
