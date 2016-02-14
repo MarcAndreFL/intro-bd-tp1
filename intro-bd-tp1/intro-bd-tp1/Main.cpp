1391440ad54793f3fc0fb8a0f7222a1f18fd6729
Marc-André Fortin-Leclerc <marc1999_8@hotmail.com>
2016-02-14 15:50:28 -0500

    Initialisation + parsing de fichier dans la classer Parser

--- intro-bd-tp1/intro-bd-tp1/Main.cpp
+++ intro-bd-tp1/intro-bd-tp1/Main.cpp
@@ -1,18 +1,9 @@
 #include "Database.h"
+#include "Parsing.h"
 #include <iostream>
 
 void main(){
-	Book myBook = Book(1, "allo", "hello", 1994);
-	Database::GetInstance()._bookTable.insert(std::pair<unsigned int, Book>(myBook.GetISBN(), myBook));
-	std::cout << Database::GetInstance()._bookTable.at(myBook.GetISBN()).GetTitle();
-
-
-	system("PAUSE");
-
-	BookLoan myLoanedBook = BookLoan(2, 1994, 4, 1, 1995, 4, 1, 10);
-	BookLoanKey bookLoanKey = BookLoanKey(myLoanedBook.GetCopyNo(), myLoanedBook.GetDateOut());
-	Database::GetInstance()._bookLoansTable.insert(std::make_pair(bookLoanKey,myLoanedBook));
-	std::cout << Database::GetInstance()._bookLoansTable.at(bookLoanKey).GetCopyNo();
-
+	Parsing parser;
+	parser.DoParsing();
 	system("PAUSE");
 }
--- intro-bd-tp1/intro-bd-tp1/Parsing.h
+++ intro-bd-tp1/intro-bd-tp1/Parsing.h
@@ -2,13 +2,21 @@
 #include <String>
 #include <Vector>
 #include <fstream>
+#include <iostream>
 #include "Database.h"
+
 
 class Parsing
 {
+private:
+	void DoBookParsing();
+	void DoBookCopyParsing();
+	void DoBookLoanParsing();
+	void DoBorrowerParsing();
 public:
 	Parsing();
 
+	void DoParsing();
 
 	~Parsing();
 };
