d34ec3b0fbcfce62a1afc052ca083da33f838e95
Marc-André Fortin-Leclerc <marc1999_8@hotmail.com>
2016-02-14 16:17:00 -0500

    Un autre exemple de requête dans le main à l'aide d'un itérateur (on est obligé pour looper dans une mapgit status)

--- intro-bd-tp1/intro-bd-tp1/Main.cpp
+++ intro-bd-tp1/intro-bd-tp1/Main.cpp
@@ -5,5 +5,17 @@
 void main(){
 	Parsing parser;
 	parser.DoParsing();
+	Database::GetInstance()._bookTable;
+	Database::GetInstance()._bookCopiesTable;
+	Database::GetInstance()._bookLoansTable;
+	Database::GetInstance()._borrowersTable;
+	std::cout << "------------------------\n\tBORROWER LIST\n------------------------\n";
+	for (std::map<unsigned int, Borrower>::iterator it = Database::GetInstance()._borrowersTable.begin(); it != Database::GetInstance()._borrowersTable.end(); it++){
+		std::cout << "------------------------\n";
+		std::cout << "Borrower Name: " << it->second.GetBorrowerName() << "\n";
+		std::cout << "Borrower No: " << it->second.GetBorrowerNo() << "\n";
+		std::cout << "Borrower Address: " << it->second.GetBorrowerAddress() << "\n";
+		std::cout << "------------------------\n";
+	}
 	system("PAUSE");
 }
